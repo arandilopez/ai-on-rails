824dcad0a1690ae3105312cdf97d2143a330130f
Arandi Lopez <arandilopez.93@gmail.com>
2024-08-13 23:01:52 -0600

    feat: seed command

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+ActiveRecord::Base.transaction do
+  ActiveRecord::Base.connection.execute(File.read(Rails.root.join("db/drugs.sql")))
+end
