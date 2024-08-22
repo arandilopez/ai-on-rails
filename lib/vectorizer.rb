b30e786e9e4e0a5fdce3922821040fb01a134a4d
Arandi Lopez <arandilopez.93@gmail.com>
2024-08-22 17:44:52 -0600

    feat: improve embedding

--- lib/vectorizer.rb
+++ lib/vectorizer.rb
@@ -1,35 +1,30 @@
 class Vectorizer
-  MODEL = "mxbai-embed-large".freeze
-  OLLAMA_ADDRESS = ENV.fetch("OLLAMA_ADDRESS", "http://host.docker.internal:11434").freeze
+  MODEL = 'mxbai-embed-large'.freeze
+  OLLAMA_ADDRESS = ENV.fetch('OLLAMA_ADDRESS', 'http://host.docker.internal:11434').freeze
 
   def initialize
     @ollama = Ollama.new(
-      credentials: { address: OLLAMA_ADDRESS, },
+      credentials: { address: OLLAMA_ADDRESS },
       options: { server_sent_events: true }
     )
   end
 
   def vectorize(text)
-    prompt = <<~PROMPT
-      Represent this sentence for searching relevant passages:
-      #{text}
-    PROMPT
-
     result = ollama.embeddings({
                                  model: MODEL,
-                                 prompt: prompt
+                                 prompt: text
                                })
 
-    result.first["embedding"]
+    result.first['embedding']
   end
 
   def vectorize_query(query)
     result = ollama.embeddings({
                                  model: MODEL,
-                                 prompt: query
+                                 prompt: "Represent this sentence for searching relevant passages: #{query}"
                                })
 
-    result.first["embedding"]
+    result.first['embedding']
   end
 
   private
